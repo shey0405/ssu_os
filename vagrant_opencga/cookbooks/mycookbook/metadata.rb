da633b9a5ce1dadd671735641a33c623c0d000bc
yoonsu <dldbstn1047@gmail.com>
2017-08-28 13:16:01 +0900

    centOS base setting vagrant file

---
+++ vagrant_opencga/cookbooks/mycookbook/metadata.rb
@@ -0,0 +1,7 @@
+name             'mycookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures mycookbook'
+long_description 'Installs/Configures mycookbook'
+version          '0.1.0'
