957af45fd7bb6168cdc4096db1108b5c7558d556
dldbstn1047 <dldbstn1234@naver.com>
2017-08-28 04:14:03 +0000

    mistake commit

--- vagrant_opencga/cookbooks/mycookbook/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'mycookbook'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures mycookbook'
-long_description 'Installs/Configures mycookbook'
-version          '0.1.0'
